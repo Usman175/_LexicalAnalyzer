508f329129554bd1a235a58fc76f40a7fe411f0d
MHFrough <mhfrough@yahoo.com>
2018-05-01 01:47:07 +0500

    Uploading Both Console + WinForm Files

---
+++ LexicalAnalyzer/LexicalAnalyzer/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace LexicalAnalyzer
+{
+	class Program
+	{
+		private static String[] separators = { ";", "{", "}", ">", "<", "+", "-", "|", "&", "~" };
+		private static String[] operators = { "()", "[]", "&&", "||", "++", "--", "==", "=<", "=>", "!=", "*", "/", "%", "(", ")", "[", "]", ",", "=" };
+		private static String[] keywords = { "abstract", "as", "base", "bool", "break", "by","byte", "case", "catch",
+			"char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double",
+			"descending", "explicit", "event", "extern", "else", "enum","false", "finally", "fixed", "float", "for",
+			"foreach", "from","goto", "group", "if", "implicit", "int", "interface","internal", "into", "is",
+			"lock", "long", "new", "null", "namespace","object", "operator", "out", "override", "orderby",  "params",
+			"private", "protected", "public", "readonly", "ref", "return","switch", "struct", "sbyte", "sealed", "short",
+			"sizeof","stackalloc", "static", "string", "select",  "this","throw", "true", "try", "typeof", "uint","ulong",
+			"unchecked","unsafe", "ushort", "using", "var", "virtual", "volatile","void", "while", "where", "yield" };
+		private static String[] comments = { "//", "/*", "*/" };
+		private static String[] constants = { "\"", "\'" };
+		private static String[] words;
+
+		static void Main(string[] args)
+		{
+			String data = System.IO.File.ReadAllText("file.txt") + "\n$~";
+
+			for (int j = 0; j < operators.Length; j++)
+				data = data.Replace(operators[j], " " + operators[j] + " ");
+
+			for (int i = 0; i < separators.Length; i++)
+				data = data.Replace(separators[i], " " + separators[i] + " ");
+
+			var newData = new StringBuilder(data);
+			for (int i = 0; i < newData.Length; i++)
+			{
+				if (newData[i] == '\n')
+					newData[i] = '$';
+			}
+			char c = '/';
+			int firstSign = 0;
+			int secondSign = 0;
+			try
+			{
+				for (int i = 0; i < newData.Length; i++)
+				{
+					if (newData[i] == c)
+					{
+						if (newData[i] == '$') { c = '/'; secondSign = i; }
+						else { c = '$'; firstSign = i; }
+						do
+						{
+							i++;
+						} while (newData[i] == '$');
+						while (firstSign <= secondSign)
+						{
+							newData[firstSign] = ' ';
+							firstSign++;
+						}
+					}
+				}
+			}
+			catch (Exception) { }
+
+			data = newData.ToString();
+			data = data.Replace("\n", String.Empty);
+			data = data.Replace("\r", String.Empty);
+			data = data.Replace("\t", String.Empty);
+			data = data.Replace("$", String.Empty);
+			data = data.Replace("~", String.Empty);
+			words = data.Split(' ');
+			for (int i = 0; i < words.Length; i++)
+				CheckLexicalAnalyzer(words[i]);
+		}
+
+		private static String Parse(String item)
+		{
+			StringBuilder str = new StringBuilder();
+
+			if (CheckSeparator(item) == true)
+				str.Append(" (separator, <" + item + ">) ");
+			else if (CheckOperators(item) == true)
+				str.Append(" (operators, <" + item + ">) ");
+			else if (CheckKeywords(item) == true)
+				str.Append(" (keywords, <" + item + ">) ");
+			else if (item.Equals("\r") || item.Equals("\n") || item.Equals("\r\n"))
+				str.Append(" (NewLine, <" + item + ">) ");
+			else
+				str.Append(" (identifier, <" + item + ">) ");
+			return str.ToString();
+		}
+
+		private static bool CheckSeparator(String str) => separators.Contains(str);
+		private static bool CheckOperators(String str) => operators.Contains(str);
+		private static bool CheckKeywords(String str) => keywords.Contains(str);
+		private static bool CheckComments(String str) => comments.Contains(str);
+		private static bool CheckConstants(String str) => constants.Contains(str);
+
+		private static void CheckLexicalAnalyzer(String str)
+		{
+			StringBuilder token = new StringBuilder();
+			bool isCheck = false;
+			for (int i = 0; i < str.Length; i++)
+			{
+				try
+				{
+					int intValue;
+					if (Int32.TryParse(str, out intValue) && !isCheck)
+					{
+						Console.WriteLine(" (integerValue, <" + str + ">) ");
+						isCheck = true;
+					}
+					else if (str.Equals("\r") || str.Equals("\n") || str.Equals("\r\n")) { }
+
+					else if (CheckSeparator(str[i].ToString()))
+						Console.WriteLine(Parse(str[i].ToString()));
+
+					else if (CheckOperators(str[i].ToString()))
+						Console.WriteLine(Parse(str[i].ToString()));
+
+					else if (str.Contains("\""))
+					{
+						if (str[i + 1].ToString() != "\"")
+							Console.WriteLine();
+						do { i++; } while (str == "\"");
+						if (i == 1)
+							Console.WriteLine(" (String, <" + str + ">) ");
+					}
+
+					else if (str.Contains("\'"))
+					{
+						if (str[i + 1].ToString() != "\'")
+							Console.WriteLine();
+						do { i++; } while (str == "\'");
+						if (i == 1)
+							Console.WriteLine(" (Char, <" + str + ">) ");
+					}
+
+					else
+					{
+						token.Append(str);
+						try
+						{
+							if (keywords.Contains(token.ToString()))
+								Console.WriteLine(Parse(token.ToString()));
+
+							else
+							{
+								int intValu;
+
+								if (!separators.Contains(str[i].ToString()))
+								if (!operators.Contains(str[i].ToString()))
+								if (!keywords.Contains(str.ToString()))
+								if (!str.Contains("\"") || !str.Contains("\'"))
+								if (!Int32.TryParse(str[i].ToString(), out intValu) && !isCheck)
+								if (!str.Equals("\r") || !str.Equals("\n") || !str.Equals('\r') || !str.Equals('\n') || !str.Equals("\r\n"))
+								{
+									Console.WriteLine(" (identifier, <" + str + ">) ");
+									isCheck = true;
+								}
+							}
+						}
+						catch (Exception) { }
+						token.Remove(i, i);
+					}
+				}
+				catch (Exception) { }
+			}
+		}
+	}
+
+}
---
+++ LexicalAnalyzerProject/LexicalAnalyzer/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LexicalAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.TextBox();
+            this.find = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.analyzed = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(12, 11);
+            this.path.Multiline = true;
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(339, 21);
+            this.path.TabIndex = 0;
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(357, 11);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(78, 23);
+            this.find.TabIndex = 1;
+            this.find.Text = "Find";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 39);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(423, 290);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "";
+            // 
+            // analyzed
+            // 
+            this.analyzed.Location = new System.Drawing.Point(441, 10);
+            this.analyzed.Name = "analyzed";
+            this.analyzed.Size = new System.Drawing.Size(222, 23);
+            this.analyzed.TabIndex = 4;
+            this.analyzed.Text = "Analyzed";
+            this.analyzed.UseVisualStyleBackColor = true;
+            this.analyzed.Click += new System.EventHandler(this.analyzed_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(441, 39);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(222, 290);
+            this.outputBox.TabIndex = 5;
+            this.outputBox.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 340);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.analyzed);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button analyzed;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
